cb2a07b9acce7a2ef1872fb01a913def9434bec0
Adrian Beściak <adrian.besciak99@gmail.com>
2019-12-16 09:03:50 +0000

    loa Main class and basic frame

---
+++ src/GUI/MyFrame.java
@@ -0,0 +1,12 @@
+package GUI;
+
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame {
+
+    public MyFrame() {
+        super("Hello World");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
