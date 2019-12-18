5b8fb2738b37c37b589b5373a6276ee90bd0f8ab
Adrian Beściak <adrian.besciak99@gmail.com>
2019-12-18 00:41:13 +0000

    added Animal constructors

--- src/main/map/animal/Animal.java
+++ src/main/map/animal/Animal.java
@@ -1,19 +1,17 @@
 package map.animal;
 
-import map.IWorldMap;
-import map.IMapElement;
-//import agh.cs.lab7.IPositionChangeObserver;
-//import project.Genome;
+import map.*;
+import map.animal.Genome;
 
 import java.lang.reflect.Array;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Animal/* implements IMapElement*/ {
-   /* private MapDirection direction;
+public class Animal implements IMapElement {
+    private MapDirection direction;
     private Vector2d position;
     private IWorldMap map;
-    private List<IPositionChangeObserver> observers = new LinkedList();
+    public Genome genome;
 
     public Animal()
     {
@@ -21,19 +19,19 @@ public class Animal/* implements IMapElement*/ {
         position = new Vector2d(2,2);
     }
 
-    public Animal(IWorldMap map)
+    public Animal(IWorldMap map)        //constructor of first animals
     {
         this();
         this.map = map;
-        this.addObserver((IPositionChangeObserver)(map));
+        genome = new Genome();
     }
 
-    public Animal(IWorldMap map, Vector2d initialPosition)
+    public Animal(IWorldMap map, Animal father, Animal mother)  //constructor of other animals
     {
         this();
         this.map = map;
-        this.position = initialPosition;
-        this.addObserver((IPositionChangeObserver)(map));
+        this.genome = new Genome(father, mother);
+        this.position = map.findPlaceForBirth(father.getPosition());
     }
 
     public Vector2d getPosition()
@@ -65,90 +63,7 @@ public class Animal/* implements IMapElement*/ {
         }
     }
 
-    private void moveForward()
-    {
-        Vector2d temp = map.calculateCorrectPositionOfElement(this.position.add(this.direction.toUnitVector()));
-        if (temp != null)
-        {
-            positionChanged(this.position, temp);
-            this.position = temp;
-        }
-    }
-
-    private void moveBackward()
-    {
-        Vector2d temp = map.calculateCorrectPositionOfElement(this.position.subtract(this.direction.toUnitVector()));
-        if (temp != null)
-        {
-            positionChanged(this.position, temp);
-            this.position = temp;
-        }
-    }
-
-    public void move(MoveDirection direction)
-    {
-        switch (direction)
-        {
-            case RIGHT:
-                this.direction = this.direction.next();
-                this.direction = this.direction.next();
-                break;
-            case LEFT:
-                this.direction = this.direction.previous();
-                this.direction = this.direction.previous();
-                break;
-            case FORWARDLEFT:
-                this.direction = this.direction.previous();
-                this.moveForward();
-                break;
-            case FORWARD:
-                this.moveForward();
-                break;
-            case FORWARDRIGHT:
-                this.direction = this.direction.next();
-                this.moveForward();
-                break;
-            case BACKWARDLEFT:
-                this.direction = this.direction.previous();
-                this.moveBackward();
-                break;
-            case BACKWARDRIGHT:
-                this.direction = this.direction.next();
-                this.moveBackward();
-                break;
-            case BACKWARD:
-                this.moveBackward();
-                break;
-            default:
-                break;
-        }
-    }
-
-    public boolean equals(Object other)
-    {
-        if (this == other)
-            return true;
-        if (!(other instanceof Animal))
-            return false;
-        Animal temp = (Animal) other;
-        return this.position.equals(temp.position) && this.direction == temp.direction;
-    }
 
 
-    public void addObserver(IPositionChangeObserver observer)
-    {
-        observers.add(observer);
-    }
 
-    public void removeObserver(IPositionChangeObserver observer)
-    {
-        observers.remove(observer);
-    }
-
-    public void positionChanged(Vector2d oldPosition, Vector2d newPosition)
-    {
-        for (IPositionChangeObserver observer : observers)
-            observer.positionChanged(oldPosition, newPosition);
-    }
-*/
 }
---
+++ src/main/map/animal/Genome.java
@@ -0,0 +1,86 @@
+package map.animal;
+
+import map.MapDirection;
+
+import java.io.Console;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class Genome{
+    public ArrayList<Integer> gens;
+    private static final int genomeSize = 32;
+    private static final int amountOfDistinctGens = 8;
+    private static final Random random = new Random();
+
+    public Genome()
+    {
+        gens = new ArrayList<Integer>();
+        for (int i = 0; i < 8; i++)
+            gens.add(i);
+        for (int i = 8; i < genomeSize; i++)
+        {
+            int value = random.nextInt(amountOfDistinctGens);
+            gens.add(value);
+        }
+        Collections.sort(gens);  
+    }
+
+    public Genome(Animal father, Animal mother)
+    {
+        int splitter1 = random.nextInt(genomeSize-1) + 1;
+        int splitter2 = random.nextInt(genomeSize-1) + 1;
+        while (splitter1 == splitter2)
+            splitter2 = random.nextInt(genomeSize-1) + 1;
+        if (splitter1 > splitter2)
+        {
+            int temp = splitter1;
+            splitter1 = splitter2;
+            splitter2 = temp;
+        }
+
+        gens = new ArrayList<Integer>();
+        for (int i = 0; i < splitter1; i++)
+            gens.add(father.genome.gens.get(i));
+        for (int i = splitter1; i < splitter2; i++)
+            gens.add(mother.genome.gens.get(i));
+        for (int i = splitter2; i < genomeSize; i++)
+            gens.add(father.genome.gens.get(i));
+        verify();
+    }
+
+    protected void verify()
+    {
+        ArrayList<Integer> counter = new ArrayList<>(
+                List.of(0, 0, 0, 0, 0, 0, 0, 0)
+        );
+
+        for (Integer gen : gens)
+        {
+            counter.set(gen, counter.get(gen)+1);
+        }
+
+        for (int i = 0; i < counter.size(); i++)
+        {
+            if (counter.get(i) == 0)
+            {
+                int j;
+                do {
+                    j = random.nextInt(genomeSize);
+                }while (counter.get(gens.get(j)) <= 1);
+                counter.set(gens.get(j), counter.get(gens.get(j)) - 1);
+                gens.set(j, i);
+            }
+        }
+        Collections.sort(gens);
+    }
+
+    public MapDirection getMove()
+    {
+        int move = gens.get(random.nextInt(genomeSize));
+        MapDirection[] dir = MapDirection.values();
+        return dir[move];
+    }
+}
+
