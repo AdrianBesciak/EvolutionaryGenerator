ab1222bbf2c5598eed194ea64c5f91cf410e3d33
Adrian Beściak <adrian.besciak99@gmail.com>
2019-12-21 18:49:43 +0000

    deleted old Swing parts of GUI

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,15 +1,5 @@
-
-import gui.MainFrame;
-
-import java.awt.EventQueue;
-
 public class Main {
     public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new MainFrame();
-            }
-        });
+       
     }
 }
